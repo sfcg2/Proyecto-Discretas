cb56525e953e759e9d158c13e521acdfce4db560
sfcg2 <castillo.sofia@correounivalle.edu.co>
2023-12-18 20:04:55 -0500

    automata

---
+++ maquina/src/Maquina.java
@@ -0,0 +1,252 @@
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Maquina {
+    static int credito = 0;
+    public void Automata(ArrayList<Integer> listaDinero){
+        App app = new App();
+        System.out.println("entro a automata " + credito);
+        ArrayList<Integer> dinero = listaDinero;
+        Estado estadoInicial = Estado.Estado_x;
+
+        
+        for(Integer d : dinero){
+            
+            switch (estadoInicial) {
+                
+                case Estado_x:
+                    System.out.println("1");
+                    switch (d) {
+                        case 500:estadoInicial = Estado.Estado_500; credito += 500; break;
+                        case 1000:estadoInicial = Estado.Estado_1000; credito += 1000; break;
+                        case 2000:estadoInicial = Estado.Estado_2000; credito += 2000; break;
+                        case 5000:estadoInicial = Estado.Estado_5000; credito += 5000; break;
+                        case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                        default:
+                            estadoInicial = Estado.Estado_Error; 
+                            break;
+                    }break;
+
+                case Estado_500:
+                    System.out.println("2");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_1000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_1500; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_2500; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_5500; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break; //+
+                            default:
+                                estadoInicial = Estado.Estado_Error;  
+                                break;
+                    }break;
+
+                case Estado_1000:
+                    System.out.println("3");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_1500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_2000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_3000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_6000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error;  
+                                break;
+                    }break;
+                case Estado_1500:
+                    System.out.println("4");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_2000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_2500; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_3500; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_6500; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }break;
+                case Estado_2000:
+                    System.out.println("5");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_2500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_3000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_4000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_7000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }break;
+                case Estado_2500:
+                    System.out.println("6");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_3000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_3500; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_4500; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_7500; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error;  
+                                break;
+                    }break;
+                case Estado_3000:
+                    System.out.println("7");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_3500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_4000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_5000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error;
+                                break;
+                    }break;
+                case Estado_3500:
+                    System.out.println("8");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_4000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_4500; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_5500; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }break;
+                case Estado_4000:
+                    System.out.println("9");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_4500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_5000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_6000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error;  
+                                break;
+                    }break;
+                
+                case Estado_4500:
+
+                    System.out.println("10");
+                    //credito = 0;
+                    
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_5000; credito = 500; break;
+                            case 1000:estadoInicial = Estado.Estado_5500; credito = 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_6500; credito = 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito = 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito = 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }
+                    break;
+                case Estado_5000:
+                    System.out.println("11");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_5500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_6000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_7000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                return;
+                    }break;
+                case Estado_5500:
+                    System.out.println("12");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_6000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_6500; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_7500; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error;  
+                                break;
+                    }break;
+                case Estado_6000:
+                   System.out.println("13");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_6500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_7000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_10000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }break;
+                case Estado_6500:
+                    System.out.println("14");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_7000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_7500; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_10000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }break;
+                case Estado_7000:
+                    System.out.println("15");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_7500; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_10000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_10000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_Error; 
+                                break;
+                    }break;
+                case Estado_7500:
+                    System.out.println("16");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_1000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_10000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_10000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_y;
+                                System.out.println("16"+ credito);  
+                                break;
+                    }
+                    break;   
+                case Estado_10000:
+                    System.out.println("17");
+                    switch (d) {
+                            case 500:estadoInicial = Estado.Estado_10000; credito += 500; break;
+                            case 1000:estadoInicial = Estado.Estado_10000; credito += 1000; break;
+                            case 2000:estadoInicial = Estado.Estado_10000; credito += 2000; break;
+                            case 5000:estadoInicial = Estado.Estado_10000; credito += 5000; break;
+                            case 10000:estadoInicial = Estado.Estado_10000; credito += 10000; break;
+                            default:
+                                estadoInicial = Estado.Estado_y;
+                                System.out.println("17 "+ credito);   
+                                break;
+                    }break;
+                case Estado_y:
+                    System.out.println("Tinto comprado -> $7500");
+                    System.out.println("Devuelta -> $" + (credito-7500) + " pesos");
+                    System.out.println("Gracias por su compra!!");
+                    break;
+
+                case Estado_Error:
+                    System.out.println("ERROR -> Dinero Insuficiente, Ingrese el faltante.\n");
+                    System.out.println("credito " + credito);
+                    app.IngresoDinero();
+                    
+                    break;
+                default:
+                    System.out.println("no entro a ninguno");
+                    break;
+            }
+            
+        }
+
+    }
+
+}
